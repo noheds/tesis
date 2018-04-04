a4951aed8023cf9a9cf1388c1dae96b2fe4c04ed
Nohelya Dos Santos <nohelyadossantos@Nohelyas-MacBook-Pro.local>
2018-04-04 11:15:57 -0500

    trululu

---
+++ Assets/Scripts/ScopeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Animator animator;
+	private bool IsScope = false;
+	public GameObject scopeoverlay;
+	public Camera maincamera;
+	public GameObject weaponcamera;
+
+	private float zoomFOV = 15f;
+	private float normalFOV ;
+
+	void Start(){
+		
+	
+	}
+	void Update(){
+	
+
+		if  (Input.GetMouseButtonDown(2)) {
+			Debug.Log("Mouse click");
+			IsScope = !IsScope;
+			animator.SetBool ("Scoped", IsScope);
+		
+			if (IsScope)
+				StartCoroutine ("OnScoped");
+			else
+				Unscoped ();
+		}
+
+	}
+
+	void Unscoped(){
+
+		scopeoverlay.SetActive (false);
+		weaponcamera.SetActive (true);
+	}
+
+	IEnumerator OnScoped(){
+	
+		yield return new WaitForSeconds (.25f);
+		scopeoverlay.SetActive (true);
+		weaponcamera.SetActive (false);
+		normalFOV = maincamera.fieldOfView;
+		maincamera.fieldOfView = zoomFOV;
+	}
+
+}
---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour {
+
+	public GameObject spawn;
+	public GameObject bulletObj;
+	public float bulletSpeed = 85000f;
+	bool canFire = true;
+	public Rigidbody rb;
+	public GameObject rifle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		FireBullet();
+	}
+
+	void FireBullet() 
+	{
+
+		if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.F)) && canFire)
+		{
+			//Create a new bullet
+			GameObject newBullet = Instantiate(bulletObj, spawn.transform.position, spawn.transform.rotation) as GameObject;
+
+			//Give it speed
+			rb = newBullet.GetComponent<Rigidbody>();
+
+			rb.AddForce(-rifle.transform.right * bulletSpeed);
+
+			//rb.AddForce(spawn.transform.position.x,spawn.transform.position.y,spawn.transform.position.z);
+
+
+			canFire = false;
+		}
+
+		//Has to release the trigger to fire again
+		if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.F))
+		{
+			canFire = true;
+		}
+	}
+}
